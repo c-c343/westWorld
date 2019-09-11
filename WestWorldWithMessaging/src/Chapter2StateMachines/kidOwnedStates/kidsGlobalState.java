c87fc887f838173dd1cdd08ff510e2436b0bcabf
c-c343 <52940123+c-c343@users.noreply.github.com>
2019-09-11 17:29:58 -0500

    add basic components for kid's Global State

--- WestWorldWithMessaging/src/Chapter2StateMachines/kidOwnedStates/kidsGlobalState.java
+++ WestWorldWithMessaging/src/Chapter2StateMachines/kidOwnedStates/kidsGlobalState.java
@@ -1,6 +1,18 @@
 package Chapter2StateMachines.kidOwnedStates;
 
 public class kidsGlobalState{
+        static final play instance = new play();
+        private play () {
+        
+        }
+ 
+ protected Object clone () throws CloneNotSupportedException{   //needed for every entity to prevent duplicates
+        throw new CloneNotSupportedException("Cloning not allowed");
+ }
+ 
+ public static play Instance (){
+        return instance;
+ }
  
 
 }
