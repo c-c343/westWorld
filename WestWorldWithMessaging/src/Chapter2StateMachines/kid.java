dbf258e2af2e09c4dc3514a17944ac51fa6a6548
c-c343 <52940123+c-c343@users.noreply.github.com>
2019-09-11 17:07:36 -0500

    Update kid.java

--- WestWorldWithMessaging/src/Chapter2StateMachines/kid.java
+++ WestWorldWithMessaging/src/Chapter2StateMachines/kid.java
@@ -1,5 +1,15 @@
 package Chapter2StateMachines;
 
-public class kid {
+import common.Messaging.Telegram;
+import Chapter2StateMachines.kidOwnedStates.kidsGlobalState;
+import Chapter2StateMachines.kidOwnedStates.play;
+import static common.windows.*;
+import static common.misc.ConsoleUtils.*;
+
+public class kid BaseGameEntity{
+  private StateMachine <kid> m_pStateMachine;
+  private location_type m_Location;
+  
+  boolean m_playing;
 
 }
